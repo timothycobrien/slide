6518069ea9b7f580591bad996c781fe69ef2f7a1
hafiz <deploy@travis-ci.org>
2020-05-14 18:54:43 +0000

    Deploy slide to github.com/ayazhafiz/slide.git:gh-pages

--- libslide/evaluator_rules/pattern_match/sidebar-items.js
+++ libslide/evaluator_rules/pattern_match/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["match_rule","Pattern matches a rule template against an expression. If successful, the results of the matching are returned as a mapping of rule to target expressions replacements."]],"struct":[["Replacements","Represents pattern-matched replacements betwen a rule and a target expression."]]});
+initSidebarItems({"struct":[["PatternMatch","Represents pattern-matched replacements betwen a rule and a target expression."]],"trait":[["MatchRule",""]]});
